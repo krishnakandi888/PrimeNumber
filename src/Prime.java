afe9167be7b7275de9548f737aac14a92eed126b
krishnareddy <krishnareddy@krishnareddy-PC>
2017-06-20 16:08:08 +0530

    firstfile change

---
+++ src/Prime.java
@@ -0,0 +1,23 @@
+public class Prime {
+	public static void primeGenerator(int n) {
+		int count = 0;
+	
+		for (int i = 2; i <= n ; i++) {
+			for (int j = 2; j <= i - 1; j++) {
+				if (i % j == 0) {
+					count = count + 1;
+				}
+			}
+			if (count == 0) {
+				System.out.print(" "+i);
+			} else {
+				count = 0;
+			}
+
+		}
+	}
+	public static void main(String[] args) {
+		
+		primeGenerator(100);
+	}
+}
